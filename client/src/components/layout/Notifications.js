11abfcf16e4c38d95da7e452dd95a9b164f3f852
traplordhuey <hupaulcamacho03@gmail.com>
2022-11-09 02:03:00 -0500

    implement notifications into app, small bug in marking them as read

--- client/src/components/layout/Notifications.js
+++ client/src/components/layout/Notifications.js
@@ -1,7 +1,128 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import PropTypes from "prop-types";
 
 // mui
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import ToolTip from "@mui/material/ToolTip";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+
+//icons
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatIcon from "@mui/icons-material/Chat";
+
+// redux
+import { connect } from "react-redux";
+import { markNotificationsRead } from "../../redux/actions/userActions";
+
+function Notifications(props) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.target);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const onMenuOpened = () => {
+    let unreadNotificationsIds = props.notifications
+      .filter((not) => !not.read)
+      .map((not) => not.notificationId);
+    props.markNotificationsRead(unreadNotificationsIds);
+  };
+
+  const notifications = props.notifications;
+  let notificationsIcon;
+
+  dayjs.extend(relativeTime);
+
+  if (notifications && notifications.length > 0) {
+    notifications.filter((not) => not.read === false).length > 0
+      ? (notificationsIcon = (
+          <Badge
+            badgeContent={notifications.filter(
+              (not) => not.read === false).length
+            }
+            color="secondary"
+          >
+            <NotificationsIcon />
+          </Badge>
+        ))
+      : (notificationsIcon = <NotificationsIcon />);
+  } else {
+    notificationsIcon = <NotificationsIcon />;
+  }
+
+  let notificationsMarkup =
+    notifications && notifications.length > 0 ? (
+      notifications.map((not) => {
+        const verb = not.type === "like" ? "liked" : "commented on";
+        const time = dayjs(not.createdAt).fromNow();
+        const iconColor = not.read ? "primary" : "secondary";
+        const icon =
+          not.type === "like" ? (
+            <FavoriteIcon color={iconColor} style={{ marginRight: 10 }} />
+          ) : (
+            <ChatIcon color={iconColor} style={{ marginRight: 10 }} />
+          );
+        return (
+          <MenuItem key={not.createdAt} onClick={handleClose}>
+            {icon}
+            <Typography
+              component={Link}
+              color="default"
+              variant="body1"
+              to={`/users/${not.recipient}/post/${not.postId}`}
+            >
+                <p>
+                    {not.sender} {verb} your post {time}.
+                </p>
+            </Typography>
+          </MenuItem>
+        );
+      })
+    ) : (
+      <MenuItem onClick={handleClose}>You have no notifications...</MenuItem>
+    );
+  return (
+    <Fragment>
+      <ToolTip placement="top" title="Notifications">
+        <IconButton
+          aria-owns={anchorEl ? "simple-menu" : undefined}
+          aria-haspopup="true"
+          onClick={handleOpen}
+        >
+          {notificationsIcon}
+        </IconButton>
+      </ToolTip>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        onEntered={onMenuOpened}
+      >
+        {notificationsMarkup}
+      </Menu>
+    </Fragment>
+  );
+}
+
+Notifications.propTypes = {
+  markNotificationsRead: PropTypes.func.isRequired,
+  notifications: PropTypes.array.isRequired,
+};
+const mapStateToProps = (state) => ({
+  notifications: state.user.notifications,
+});
+
+export default connect(mapStateToProps, { markNotificationsRead })(
+  Notifications
+);
